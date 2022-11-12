9f9224e8bc8ef9afaf822d5fbc5df83c8128d30a
GiannyFrankGaleano <giannyfrank11@gmail.com>
2022-11-12 08:53:15 -0500

    Actualizacion del proyecto sobre el consecionario

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,22 @@
 import personas.Empleado;
 import personas.tipos.tipoDocumento;
 import personas.tipos.tipoPermiso;
+import vehiculos.Particular;
+import vehiculos.Vehiculo;
+import vehiculos.tipos.TipoMarca;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Main {
 
     private static ArrayList<Empleado> empleados = new ArrayList<>();
+    private static ArrayList<Vehiculo> vehiculos = new ArrayList<>();
 
     public static void main(String[] args) {
-        System.out.println("-----------------------------");
-        System.out.println("--------------Concesionario UDEV--------------");
-        System.out.println("-----------------------------");
 
 
+        generarEmpleados();
     }
 
     private static void generarEmpleados() {
@@ -32,10 +35,104 @@ public class Main {
 
         tercerEmpleado.setTipoPermiso(tipoPermiso.TODOS);
 
+        Empleado cuartoEmpleado = new Empleado("Mateo", 32, "111236654", tipoDocumento.CEDULA, "31231323232", "Carrera 123A #18-23");
+
+        cuartoEmpleado.setTipoPermiso(tipoPermiso.COMPRA);
+
         empleados.add(primerEmpleado);
         empleados.add(segundoEmpleado);
         empleados.add(tercerEmpleado);
+        empleados.add(cuartoEmpleado);
 
 
+    }
+
+    public static void menuPrincipal() {
+        System.out.println("------------------------------------------------");
+        System.out.println("--------------Concesionario UDEV--------------");
+        System.out.println("------------------------------------------------");
+        String mensaje = "Elija una opcion \n"
+                + "1. Venta de vehiculo \n"
+                + "2. Ingreso de Vehiculo\n"
+                + "3. Salir";
+
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog(mensaje));
+        switch (opcion) {
+
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+            default:
+        }
+    }
+
+    private static void ventaVehiculo() {
+
+        if (vehiculos.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Error; no hay vehiculos en la lista", "VEHICULOS NO DISPONIBLES", JOptionPane.ERROR_MESSAGE);
+
+
+
+        } else {
+
+        }
+
+    }
+
+    private static void IngresarVehiculo() {
+        TipoMarca marca;
+        int modelo;
+        int precio;
+        String matricula;
+        String color;
+
+        if (preguntaVehiculo()){
+            Particular particular = new Particular(TipoMarca.BENTLEY, 2021, 400000000, "OMG007", "Negro");
+        }
+    }
+
+    private static boolean preguntaVehiculo() {
+        int tipo = Integer.parseInt(JOptionPane.showInputDialog("Que tipo de vehiculo desea?\n"
+                + "1. particular\n" + "2. Publico"));
+        return tipo == 1;
+    }
+
+    private static TipoMarca elegirMarcaVehiculo() {
+        TipoMarca NewMarca = null;
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la marca que desea comprar: \n"
+                + "1. CHEVROLET\n"
+                + "2. FORD\n"
+                + "3. PORCHE\n"
+                + "4. BENTLEY\n"
+                + "5. TOYOTA"));
+
+        switch (opcion) {
+            case 1:
+                NewMarca = TipoMarca.CHEVROLET;
+                break;
+            case 2:
+                NewMarca = TipoMarca.FORD;
+                break;
+            case 3:
+                NewMarca = TipoMarca.PORCHE;
+                break;
+            case 4:
+                NewMarca = TipoMarca.BENTLEY;
+                break;
+
+            case 5:
+                NewMarca = TipoMarca.TOYOTA;
+                break;
+
+            default:
+            JOptionPane.showMessageDialog(null,"Opcion no valida","ERROR",JOptionPane.ERROR_MESSAGE);
+
+            elegirMarcaVehiculo();
+        }
+        return NewMarca;
     }
 }
